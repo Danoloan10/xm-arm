21d7ce3ac5e58960a23a18e59f86e50bee209142
danoloan10 <danoloan10@tutanota.com>
2021-06-09 22:34:50 +0200

    Esqueleto de implementación de stdlib extendido

--- user/xal/include/string.h
+++ user/xal/include/string.h
@@ -22,22 +22,24 @@ extern "C" {
 #include <xm.h>
 #include <stddef.h>
 
-extern void *memset(void *dst, xm_s32_t s, size_t count);
-extern void *memcpy(void *dst, const void* src, size_t count);
 extern ssize_t memcmp(const void *dst, const void *src, size_t count);
-extern char *strcpy(char *dst, const char *src);
-extern char *strncpy(char *dest, const char *src, xmSize_t n);
-extern char *strcat(char *s, const char* t);
-extern char *strncat(char *s, const char *t, xmSize_t n);
-extern ssize_t strcmp(const char *s, const char *t);
-extern ssize_t strncmp(const char *s1, const char *s2, xmSize_t n);
-extern size_t strlen(const char *s);
-extern char *strrchr(const char *t, xm_s32_t c);
-extern char *strchr(const char *t, xm_s32_t c);
-extern char *strstr(const char *haystack, const char *needle);
-extern void *memmove(void *dst, const void *src, xmSize_t count);
+extern void *  memcpy(void *dst, const void* src, size_t count);
+extern void *  memmove(void *dst, const void *src, size_t count);
+extern void *  memset(void *dst, xm_s32_t s, size_t count);
 
-extern size_t strnlen(const char *s, size_t len);
+extern char *  strcat(char *s, const char* t);
+extern ssize_t strcmp(const char *s, const char *t);
+extern char *  strcpy(char *dst, const char *src);
+extern size_t  strlen(const char *s);
+
+extern char *  strncat(char *s, const char *t, size_t n);
+extern ssize_t strncmp(const char *s1, const char *s2, size_t n);
+extern char *  strncpy(char *dest, const char *src, size_t n);
+extern size_t  strnlen(const char *s, size_t len);
+
+extern char *  strchr(const char *t, xm_s32_t c);
+extern char *  strrchr(const char *t, xm_s32_t c);
+extern char *  strstr(const char *haystack, const char *needle);
 
 #ifdef __cplusplus
 }
